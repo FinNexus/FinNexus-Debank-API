1a362f26a5061f6f08fe06d0f16caa1b20461348
jiaqinggang1 <forestjqg@126.com>
2021-03-12 17:24:57 +0800

    changed debank view function

--- contracts/FnxMineDebankView.sol
+++ contracts/FnxMineDebankView.sol
@@ -29,26 +29,26 @@ interface IMineConverter {
      function lockedBalanceOf(address account) external view returns (uint256);
 }
 
-contract FnxMineDebankView is Storage,Ownable {
-    
+contract FnxMineDebankView is Ownable {
     using SafeMath for uint256;
 
-    function getMinedUnclaimedBalance(address _user) public view returns (uint256) {
-        return IFixedMinePool(fixedMinePool).getMinerBalance(_user,cfnxToken);
+    address public fnxColPool = 0xfDf252995da6D6c54C03FC993e7AA6B593A57B8d;
+    address public fnxToken = 0xeF9Cd7882c067686691B6fF49e650b43AFBBCC6B;
+    address public cfnxToken = 0x9d7beb4265817a4923FAD9Ca9EF8af138499615d;
+    address public fnxOracle = 0x43BD92bF3Bb25EBB3BdC2524CBd6156E3Fdd41F3;
+
+    function getMinedUnclaimedBalance(address _user,address minepool) public view returns (uint256) {
+        return IFixedMinePool(minepool).getMinerBalance(_user,cfnxToken);
     }
 
-    function getConverterLockedBalance(address _user) public view returns (uint256) {
-        return IMineConverter(cfnxToken).lockedBalanceOf(_user);
-    }
-
-    
-    function getApy(address _user) public view returns (uint256) {
-            uint256 mineofyear = IFixedMinePool(fixedMinePool).getUserCurrentAPY(_user,cfnxToken);
+   function getApy(address _user,address minepool,address colforfptapool) public view returns (uint256) {
+            uint256 mineofyear = IFixedMinePool(minepool).getUserCurrentAPY(_user,cfnxToken);
             
-            uint256 FTPA = IFixedMinePool(fixedMinePool).getUserFPTABalance(_user);
-            uint256 FTPB = IFixedMinePool(fixedMinePool).getUserFPTBBalance(_user);
+            uint256 FTPA = IFixedMinePool(minepool).getUserFPTABalance(_user);
+            uint256 FTPB = IFixedMinePool(minepool).getUserFPTBBalance(_user);
             uint256 fnxprice =  IFnxOracle(fnxOracle).getPrice(fnxToken);
-            uint256 fptaprice = ICollateralPool(usdcColPool).getTokenNetworth();
+
+            uint256 fptaprice = ICollateralPool(colforfptapool).getTokenNetworth();
             uint256 fptbprice = ICollateralPool(fnxColPool).getTokenNetworth();
 
             uint256 denominater = (FTPA.mul(fptaprice)).add(FTPB.mul(fptbprice));
@@ -59,54 +59,33 @@ contract FnxMineDebankView is Storage,Ownable {
             
             return mineofyear.mul(fnxprice).mul(1000).div(denominater);
     }
-    
-    
-    function getFnxPoolColValue(address _user) public view returns (uint256) {
-       return ICollateralPool(fnxColPool).getUserPayingUsd(_user);
-    }
 
-
-    function getUsdcPoolColValue(address _user)  public view returns (uint256) {
-        return ICollateralPool(usdcColPool).getUserPayingUsd(_user);
-    }
-    
     /**
      * @dev Retrieve user's locked balance. 
      * @param _user account.
      * @param _collateral the collateal token address
-     * @param _pool the collateal pool     
+     * @param _colpool the collateal pool
      */
-    function getUserInputCollateral(address _user,address _collateral,address _pool) public view returns (uint256){
-      return ICollateralPool(_pool).userInputCollateral(_user,_collateral);   
+    function getUserInputCollateral(address _user,address _collateral,address _colpool) public view returns (uint256){
+      return ICollateralPool(_colpool).userInputCollateral(_user,_collateral);
     }
     
     function getVersion() public pure returns (uint256)  {
         return 1;
     }
-    
 
     function resetTokenAddress( 
                                 address _fnxColPool, 
-                                address _usdcColPool, 
-                                address _fnxToken,   
-                                address _usdcToken, 
-                                address _usdtToken,
+                                address _fnxToken,
                                 address _cfnxToken,
-                                address _fnxOracle,
-                                address _fixedMinePool
-                                
+                                address _fnxOracle
                               )  public onlyOwner {
                                   
         fnxColPool  = _fnxColPool;
-        usdcColPool = _usdcColPool;
-        fnxToken    = _fnxToken; 
-        usdcToken   = _usdcToken;
-        usdtToken   = _usdtToken;
+        fnxToken    = _fnxToken;
         cfnxToken   = _cfnxToken;
         fnxOracle    = _fnxOracle;
-        fixedMinePool = _fixedMinePool;
     }
-    
     
 }
 
